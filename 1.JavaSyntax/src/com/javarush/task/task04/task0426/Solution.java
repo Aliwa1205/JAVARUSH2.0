5927b0f97e64dea7ec05049cf76a1ce1b5913662
Alina <APetryshche@platinumbank.com.ua>
2017-02-08 14:04:44 +0200

    new add

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0426/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task04.task0426;
+
+/* 
+Ярлыки и числа
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))
+
+        String var = bufferedReader.readLine();
+        int a = Integer.parseInt(var);
+
+        if (a==0)
+        {
+            System.out.println("ноль");
+            else if (a>0&&a%2==0)
+        }
+    }
+}
