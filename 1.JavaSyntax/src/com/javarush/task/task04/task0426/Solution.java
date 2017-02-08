6bc0a26a743a7702f82a86bcb0e5f9c21124a7a9
Alina <APetryshche@platinumbank.com.ua>
2017-02-08 14:57:18 +0200

    new add

--- 1.JavaSyntax/src/com/javarush/task/task04/task0426/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0426/Solution.java
@@ -8,15 +8,52 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         String var = bufferedReader.readLine();
         int a = Integer.parseInt(var);
 
-        if (a==0)
-        {
+        Solution solution = new Solution();
+
+
+        if (a == 0) {
             System.out.println("ноль");
-            else if (a>0&&a%2==0)
+            return;
         }
+
+
+        if (solution.isPositiv(a))
+        {
+            if (isEven(a)) {
+                System.out.println("положительное четное число");
+            }
+            else
+            {
+                System.out.println("положительное нечетное число");
+            }
+        }
+        else
+        {
+            if(isEven(a))
+            {
+                System.out.println("отрицательное четное число");
+            }
+            else
+            {
+                System.out.println("отрицательное нечетное число");
+            }
+        }
+
+
+    }
+
+    private boolean isPositiv(int a)
+    {
+        return a>0;
+    }
+
+    private static boolean isEven(int a)
+    {
+        return a%2==0;
     }
 }
